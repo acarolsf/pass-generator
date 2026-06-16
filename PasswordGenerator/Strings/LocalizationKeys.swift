e5622f648a5201109338d5e21cba77debcfd1129
Ana Carolina <cscarol20@gmail.com>
2026-06-16 02:09:25 -0300

    Update localization keys

---
+++ PasswordGenerator/Strings/LocalizationKeys.swift
@@ -0,0 +1,49 @@
+//
+//  Constants.swift
+//  PasswordGenerator
+//
+//  Created by Ana Carolina Ferreira on 04/08/22.
+//
+
+import Foundation
+
+enum LocalizationKeys {
+    static let appName = "app_name"
+    static let numberOfPasswords = "number_of_passwords"
+    static let totalOfCharacteres = "total_of_characteres"
+    static let lowercasedLetters = "lowercased_letters"
+    static let uppercasedLetters = "uppercased_letters"
+    static let useNumbers = "use_numbers"
+    static let useSpecialCharacteres = "use_special_characteres"
+    static let generate = "generate"
+    static let allFieldsHasToBeFilled = "all_fields_has_to_be_filled"
+    static let listOfPasswords = "list_of_passwords"
+    static let copyToClipboard = "copy_to_clipboard"
+    static let copiedToClipboard = "copied_to_clipboard"
+    static let generateNewPasswords = "generate_new_passwords"
+    static let securityTips = "security_tips"
+    static let chooseSafePassword = "choose_safe_password"
+    static let unfilledForm = "unfilled_form"
+    static let save = "save"
+    static let cancel = "cancel"
+    static let ok = "ok"
+    
+    static let savePassword = "save_password"
+    static let descriptionTextFieldView = "description_textField_view"
+    
+    // MARK: - Size of passwords
+    static let shortPassword = "short_password"
+    static let mediumPassword = "medium_password"
+    static let largePassword = "large_password"
+    static let superLargePassword = "super_large_password"
+    
+    // Tips
+    static let tipOneTitle = "tip_one_title"
+    static let tipOneText = "tip_one_text"
+    static let tipTwoTitle = "tip_two_title"
+    static let tipTwoText = "tip_two_text"
+    static let tipThreeTitle = "tip_three_title"
+    static let tipThreeText = "tip_three_text"
+    static let tipFourTitle = "tip_four_title"
+    static let tipFourText = "tip_four_text"
+}
